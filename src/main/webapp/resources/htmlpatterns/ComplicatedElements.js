a1fe1ac6213f700a70dee5af89807fcc270ccaa6
Bogdan Hadzhiev <bogdanhadzhiev@yahoo.com>
2016-04-11 20:51:52 +0300

    Add complicated elements generation

---
+++ src/main/webapp/resources/htmlpatterns/ComplicatedElements.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Gadzzzz on 11.04.2016.
+ */
+var loginForm = [
+    {name:"label",top:250,left:350,val:"LOGIN FORM:"},
+    {name:"label",top:300,left:300,val:"login:"},
+    {name:"field",top:300,left:370,val:"login"},
+    {name:"label",top:350,left:300,val:"password:"},
+    {name:"field",top:350,left:370,val:"password"},
+    {name:"button",top:400,left:370,val:"login"},
+];
+
+var registerForm = [
+    {name:"label",top:250,left:340,val:"REGISTER FORM:"},
+    {name:"label",top:300,left:300,val:"login:"},
+    {name:"field",top:300,left:370,val:"login"},
+    {name:"label",top:350,left:300,val:"password:"},
+    {name:"field",top:350,left:370,val:"password"},
+    {name:"label",top:400,left:300,val:"retry pass:"},
+    {name:"field",top:400,left:370,val:"retry pass"},
+    {name:"label",top:450,left:300,val:"email:"},
+    {name:"field",top:450,left:370,val:"email"},
+    {name:"button",top:500,left:370,val:"register"},
+];
+
+var newsForm = [
+    {name:"label",top:250,left:360,val:"NEWS FORM:"},
+    {name:"label",top:300,left:300,val:"title:"},
+    {name:"field",top:300,left:390,val:"title"},
+    {name:"label",top:350,left:300,val:"description:"},
+    {name:"textarea",top:350,left:390,val:"description"},
+    {name:"checkbox",top:450,left:300},
+    {name:"checkbox",top:450,left:380},
+    {name:"checkbox",top:450,left:460},
+    {name:"button",top:500,left:390,val:"add"},
+];
+
+
